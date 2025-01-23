0d345d9f6c308b6e7cc1f457b185b7a1d10f7e5d
Conq <76233356+ConqAra@users.noreply.github.com>
2025-01-22 23:36:07 -0500

    fix r2h.py
    made dark theme optional.
    properly added っ, じ, し, and た.
    added numbers and more punctuation.
    fixed error with hiragana characters in-between compounds not working.

--- r2h.py
+++ r2h.py
@@ -29,6 +29,7 @@ hiragana = {
     'zu': 'ず',
     'ze': 'ぜ',
     'zo': 'ぞ',
+    'chi': 'た',
     'ta': 'た',
     'ti': 'ち',
     'tu': 'つ',
@@ -80,8 +81,26 @@ hiragana = {
     '.':'。',
     '!': '！',
     '?': '？',
-    '~' : 'っ',
-    'vu' : 'ゔ'
+    ',': '、',
+    '~': '〜',
+    '-': 'ー',
+    '0': '０',
+    '1': '１',
+    '2': '２',
+    '3': '３',
+    '4': '４',
+    '5': '５',
+    '6': '６',
+    '7': '７',
+    '8': '８',
+    '9': '９',
+    '%': '％',
+    '(': '（',
+    ')': '）',
+    'tt' : 'っ',
+    'vu' : 'ゔ',
+    'ji' : 'じ',
+    'shi': 'し'
 }
 
 compounds = {
@@ -124,17 +143,20 @@ compounds = {
 }
 
 def r2h(input_string):
+    input_string = input_string.lower().replace(" ", "")
+
     for compound, hiragana_char in compounds.items():
         input_string = input_string.replace(compound, compound[0] + compound[1:].lower())
 
-    word_list = re.findall(r'(tsu|tu|[kgztdnhbpmyrwvs][aiueo]|[aiueon~.!?]|[kgyshjcdtbnmry][yaoue]{2,3})', input_string)
-
+    word_list = re.findall(r'(ky[auo]|gy[auo]|sh[auo]|j[auo]|ch[iauo]|jy[auo]|ny[auo]|hy[auo]|by[auo]|py[auo]|my[auo]|ry[auo]|shi|tsu|tu|[kgztdnhbpmyrwvsj][aiueot]|[aiueon~.!?,-~0123456789)(]|[kgyshjcdtbnmry][yaoue]{2,3})', input_string)
+    
     max_len = sum(len(word) for word in word_list)
     if max_len != len(input_string):
         messagebox.showerror("error", "invalid input string!")
         return ""
 
     output_string = ""
+    
     for word in word_list:
         if word == 'tsu' or word == 'tu':
             output_string += hiragana['tsu']
@@ -154,20 +176,21 @@ def on_convert_button_click(event=None):
     else:
         messagebox.showwarning("error", "no text")
 
-# gui
+# gui, uncomment for dark theme
 root = tk.Tk()
 root.title("romanji to hiragana converter")
-style = AwthemesStyle(root)
-themes = style.theme_names()
-style.theme_use("awdark")
-root.configure(bg=style.lookup("TFrame", "background"))
+# style = AwthemesStyle(root)
+# themes = style.theme_names()
+# style.theme_use("awdark")
+# root.configure(bg=style.lookup("TFrame", "background"))
 font_style = ('Arial', 18)
 
 input_entry = tk.Entry(root, width=20, font=font_style)
 input_entry.pack(pady=10)
 input_entry.bind('<Return>', on_convert_button_click)
 
-convert_button = tk.Button(root, text="convert", command=on_convert_button_click, font=('Arial', 12), bg=style.lookup("TButton", "background"), fg=style.lookup("TButton", "foreground"))
+convert_button = tk.Button(root, text="convert", command=on_convert_button_click, font=('Arial', 12)#, bg=style.lookup("TButton", "background"), fg=style.lookup("TButton", "foreground")
+)
 convert_button.pack(pady=10)
 
 result_text = tk.Text(root, height=4, width=40, wrap=tk.WORD, font=font_style)
